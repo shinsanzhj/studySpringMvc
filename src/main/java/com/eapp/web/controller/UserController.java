f665937989d1780feecc5a7ac08e5a132343b575
shinsan_zhj <mindofzhj@163.com>
2016-03-29 12:19:55 +0800

    添加Servlet

--- src/main/java/com/eapp/web/controller/UserController.java
+++ src/main/java/com/eapp/web/controller/UserController.java
@@ -1,7 +1,9 @@
 package com.eapp.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
@@ -10,5 +12,11 @@ public class UserController {
 	@RequestMapping("/helloWorld")
 	public String helloWorld() {
 		return "user/helloWorld";
+	}
+	
+	@RequestMapping("/getRequestBody")
+	public String getRequestBody(@RequestParam String userId) {
+		System.out.println("userId:" + userId);
+		return "index";
 	}
 }
